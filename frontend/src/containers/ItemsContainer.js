6c76651830a4075cb928dbf0e6c080e5d83b740b
Ben Hayes <b.t.hayes@comcast.net>
2021-09-21 16:55:48 -0500

    added redux

---
+++ frontend/src/containers/ItemsContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import ItemInput from '../components/items/ItemInput';
+//import Items from '../components/items/Items';
+import { connect } from 'react-redux';
+
+class ItemsContainer extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+      </div>
+    )
+  }
+}
+
+export default ItemsContainer;
